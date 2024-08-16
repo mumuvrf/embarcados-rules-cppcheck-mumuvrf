058d92560136604109213b27e8ff4425e31ab87e
Vinícius "Mumu" Rodrigues <116031133+mumuvrf@users.noreply.github.com>
2024-08-16 14:44:48 -0300

    Bugs fixed at main.c

--- main.c
+++ main.c
@@ -5,15 +5,18 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 4, b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
+    printf("a x b = %d\n", result);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
+        printf("i: %d\n", arr[i]);
     }
 
     return 0;
